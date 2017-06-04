1eb6c72080d71ca998d9ba6c748aace368aaba4e
Luz Gutierrez <mili01gm@gmail.com>
2017-06-04 13:19:40 -0500

    Listo slide automático y botones prev/next. Faltan bullets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,65 @@
-$(() => {
+jQuery(function($) {
+
+    // Variables//////////////////////////////////////////////////
+    var $slider = $('.slides');
+    var $slide = 'img';
+    var $transition_time = 800;
+    var $time_between_slides = 4000;
+
+    function slides() {
+        return $slider.find($slide);
+    }
+
+    //Auto animation//////////////////////////////////////////////
+    slides().fadeOut();
+
+    slides().first().addClass('active');
+    slides().first().fadeIn($transition_time);
+
+    $interval = setInterval(
+        function() {
+
+            var $i = $slider.find($slide + '.active').index();
+
+            slides().eq($i).removeClass('active');
+            slides().eq($i).fadeOut($transition_time);
+
+            if (slides().length == $i + 1) $i = -1; // To first image
+
+            slides().eq($i + 1).fadeIn($transition_time);
+            slides().eq($i + 1).addClass('active');
+        }, $transition_time + $time_between_slides
+    );
+
+    //Next button/////////////////////////////////////////////////
+    $("#next").on('click', function(e) {
+        e.preventDefault();
+
+        var $i = $slider.find($slide + '.active').index();
+
+        slides().eq($i).removeClass('active');
+        slides().eq($i).fadeOut($transition_time);
+
+        if (slides().length == $i + 1) $i = -1; // To first image
+
+        slides().eq($i + 1).fadeIn($transition_time);
+        slides().eq($i + 1).addClass('active');
+
+    });
+
+    //Previous button/////////////////////////////////////////////
+    $("#prev").on('click', function(e) {
+        e.preventDefault();
+
+        var $i = $slider.find($slide + '.active').index();
+
+        slides().eq($i).removeClass('active');
+        slides().eq($i).fadeOut($transition_time);
+
+        if (slides().length == $i) $i = -1; // To first image
+
+        slides().eq($i - 1).fadeIn($transition_time);
+        slides().eq($i - 1).addClass('active');
+    });
 
 });
